79da72c3ff943ec32e6dddf6186ed9e14dde5967
Guillaume Simard <guillaume.simard.sdec@gmail.com>
2016-10-25 15:08:34 -0400

    Début

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
-package sample;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -10,9 +9,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Group root = new Group();
+        primaryStage.setTitle("Wandermys");
+        primaryStage.setScene(new Scene(root, 1366, 768));
         primaryStage.show();
     }
 
